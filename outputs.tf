7380b90e9c1210cace903bba561655454011df6c
gywilmarroquin <gywilmarroquin@micoopeguayacan.gt>
2025-08-22 16:21:38 -0600

    status

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,10 @@
 output "droplet_ip" {
   description = "La dirección IP pública del droplet"
-  value       = digitalocean_droplet.mi_droplet.ipv4_address
+  value       = digitalocean_droplet.laravel_app.ipv4_address
 }
 
 output "droplet_name" {
   description = "El nombre del droplet desplegado"
-  value       = digitalocean_droplet.mi_droplet.name
+  value       = digitalocean_droplet.laravel_app.name
 }
 
-output "droplet_status" {
-  description = "Estado del droplet (active, off, archive)"
-  value       = digitalocean_droplet.mi_droplet.status
-}
